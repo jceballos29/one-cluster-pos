e38165547c2e415fe1527170376dedc65600675f
Juan Ceballos <jacu29@gmail.com>
2023-03-03 15:40:27 -0500

    Create Routes for elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
 /** @format */
 
 import {
+	BrowserRouter,
 	Navigate,
 	Outlet,
+	Route,
 	RouterProvider,
+	Routes,
 	createBrowserRouter,
 } from 'react-router-dom';
 import { PrivateRoutes, PublicRoutes } from './constants/routes';
 import { Login, PointOfSale } from './pages';
 
-let router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Navigate to={PrivateRoutes.POS} />,
-	},
-	{
-		path: PublicRoutes.LOGIN,
-		element: <Login />,
-	},
-	{
-		path: PrivateRoutes.POS,
-		element: <PointOfSale />,
-	},
-]);
-
-if (import.meta.hot) {
-	import.meta.hot.dispose(() => router.dispose());
-}
+// let router = createBrowserRouter([
+// 	{
+// 		path: '/',
+// 		element: <Navigate to={PrivateRoutes.POS} />,
+// 	},
+// 	{
+// 		path: PublicRoutes.LOGIN,
+// 		element: <Login />,
+// 	},
+// 	{
+// 		path: PrivateRoutes.POS,
+// 		element: <PointOfSale />,
+// 	},
+// ]);
 
 export default function App() {
 	return (
 		<main className='w-full h-screen overflow-hidden bg-blue-50'>
-			<RouterProvider router={router} />
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path='/'
+						element={<Navigate to={PrivateRoutes.POS} />}
+					/>
+					<Route path={PublicRoutes.LOGIN} element={<Login />} />
+					<Route path={PrivateRoutes.POS} element={<PointOfSale />} />
+				</Routes>
+			</BrowserRouter>
 		</main>
 	);
 }
