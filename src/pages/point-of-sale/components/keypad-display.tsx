df46e4c52af4e8e8d77974995785dc70a51bf3ec
Juan David Salazar Santa <juandsalazars@gmail.com>
2023-05-09 18:05:25 -0500

    Change the color of the display to black to improve contrast

--- src/pages/point-of-sale/components/keypad-display.tsx
+++ src/pages/point-of-sale/components/keypad-display.tsx
@@ -7,10 +7,10 @@ const KeypadDisplay = () => {
 
     return (
         <div className='relative flex flex-row justify-end items-end pb-4 pr-4 w-full h-32
-            bg-blue-200 rounded-xl not-italic text-4xl leading-7 text-white'
+            bg-blue-200 rounded-xl not-italic text-4xl leading-7 text-black'
         >
             <div className='absolute inset-0 flex flex-row justify-center items-center m-3 w-20 h-10
-            bg-orange-200 rounded-xl not-italic font-bold text-xl text-white'>
+            bg-orange-200 rounded-xl not-italic font-bold text-xl text-black'>
                 {type}
             </div>
             {discountOption === 'Valor' ?
